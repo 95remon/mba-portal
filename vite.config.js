8dd25b5c26098dca0b576397c2ff3021b8cc19a9
remon <95remon@gmail.com>
2026-02-08 19:56:53 +0300

    Switch to relative paths in config and HTML

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/mba-portal/',
+    base: './', // Use relative paths for maximum compatibility
     build: {
         outDir: 'dist',
     }
